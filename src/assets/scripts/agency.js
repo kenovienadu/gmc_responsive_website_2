d4a091fea6430075c5ccba76090ab43de962ccf8
Ken Ovienadu <ken.ovienadu@upflex.com>
2024-10-25 00:58:38 +0100

    feat: implement responsive agency website

--- src/assets/scripts/agency.js
+++ src/assets/scripts/agency.js
@@ -3,25 +3,25 @@ const items = [
     "title": "Santorini",
     "description": "A stunning island in Greece known for its white-washed buildings with blue domes, crystal-clear waters, and breathtaking sunsets. It's famous for the caldera view and picturesque villages like Oia and Fira.",
     "price": "$1500",
-    "image": "https://placehold.co/600x400?font=roboto&text=Santorini"
+    "image": "https://images.unsplash.com/photo-1696519669687-44a05404eadf?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     "title": "Barcelona",
     "description": "A vibrant city in Spain known for its unique architecture by Antoni Gaudí, including La Sagrada Familia and Park Güell. Barcelona also offers lively markets, beaches, and cultural attractions.",
     "price": "$1200",
-    "image": "https://placehold.co/600x400?font=roboto&text=Barcelona"
+    "image": "https://images.unsplash.com/photo-1464730282481-19bd52679224?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     "title": "Alhambra, Granada",
     "description": "A magnificent palace and fortress complex in southern Spain. Alhambra is renowned for its stunning Moorish architecture, lush gardens, and views of the Sierra Nevada mountains.",
     "price": "$300",
-    "image": "https://placehold.co/600x400?font=roboto&text=Granada"
+    "image": "https://images.unsplash.com/photo-1564740603199-5f56138c6679?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     "title": "Ibiza",
     "description": "An island in Spain’s Balearic Islands, famous for its world-class nightlife, stunning beaches, and crystal-clear waters. Ibiza is a top destination for partygoers and nature lovers alike.",
     "price": "$1800",
-    "image": "https://placehold.co/600x400?font=roboto&text=Ibiza"
+    "image": "https://plus.unsplash.com/premium_photo-1697730002753-6772fea94e55?q=80&w=1738&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   }
 ]
 
@@ -48,7 +48,7 @@ const loadProducts = () => {
             ${product.description}
           </p>
           <button class="primary-button">
-            See More
+            Learn More
           </button>
         </div>
       </div>
